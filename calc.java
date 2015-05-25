b82b514074ad68e74c1479974d5c8109d5d7c626
Fanker1 <Fank@mail.ru>
2015-05-25 17:10:47 +0300

    new bidlo calc

--- calc.java
+++ calc.java
@@ -1,5 +1,26 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+
+
 public class calc{
 	public static void main(String[]args){
-		System.out.println("Calculate...");
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
+        System.out.print("#");
+        int a = sc.nextInt();
+        System.out.print(" + - * /");
+        String b = br.readLine();
+        System.out.print("#");
+        int d = sc.nextInt();
+        int r = 0;
+            if (b.equals("+"))r = a + d;
+            else if(b.equals("-")) r = a - d;
+            else if(b.equals("*")) r = a * d;
+            else if(b.equals("/")) r = a / d;
+        System.out.print("result ");
+        System.out.println(r);
 	}
 }
